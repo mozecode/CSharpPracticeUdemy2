4c24a80fde01b815fa792ecb71f54f2d22e31ea1
Greta Moseley <greta.moseley@gmail.com>
2017-12-19 12:19:08 -0600

    add practice exercise from Udemy on CSharp using array, min/max

--- CSharpPracticeApp2/Program.cs
+++ CSharpPracticeApp2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,32 @@ namespace CSharpPracticeApp2
     class Program
     {
         static void Main(string[] args)
-        {
+        {//Write a program which takes two numbers from the console and displays the maximum of the two.
+
+            double[] numArr = new double[2]; //initialize a number array to catch values.  
+
+            for (var i = 0; i <numArr.Length; i++) //create a loop to ask for values and do validation with TryParse
+            {
+                Console.WriteLine("Please enter a number: ");
+               
+                while (double.TryParse(Console.ReadLine(), out numArr[i]) == false || numArr[i]<0) //protect against bad input
+                {
+                    Console.WriteLine("Sorry, invalid input.  Please try again.");
+                }
+
+
+                Console.WriteLine("You entered {0}", numArr[i]);
+            }
+
+            //determine which number in the array is the max and min
+
+            var max = numArr.Max();
+            var min = numArr.Min();
+
+            Console.WriteLine("The highest number was {0}", max);
+            Console.WriteLine("The lowest number was {0}", min);
+            Console.ReadKey();
+
         }
     }
 }
